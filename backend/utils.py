43100123cde1509b88d5bd16f06867ca35fa9894
renanxcortes <renanxcortes@gmail.com>
2019-01-28 09:06:34 -0800

    changing 'barragem' to 'dam' in the back-end

--- backend/utils.py
+++ backend/utils.py
@@ -4,7 +4,7 @@ from scipy import interpolate
 x = np.linspace(-20.139558, -20.115769, 1000)
 y = np.linspace(-44.141856, -44.099738, 1000)
 z = np.array([100.0, 50.0, 50.0, 25, 100.0])
-barragem = [-20.119026, -44.119985]
+dam = [-20.119026, -44.119985]
 X, Y = np.meshgrid(x, y)
 npts = 5
 px, py = np.random.choice(x, npts), np.random.choice(y, npts)
@@ -27,7 +27,7 @@ class Position:
         self.lng = lng
 
     def calc_vector(self):
-        v_pos_x, v_pos_y = (self.lat - barragem[0], self.lng - barragem[1])
+        v_pos_x, v_pos_y = (self.lat - dam[0], self.lng - dam[1])
         d = np.sqrt((v_pos_x ** 2 + v_pos_y ** 2))
         print(v_pos_x, v_pos_y, d)
         xf = self.lat + v_pos_x
