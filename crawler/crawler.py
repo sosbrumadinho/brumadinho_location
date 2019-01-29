8be779c17192e158ac91d631111864136efc8d3c
BrunaDolavale <brunadolavalerj@gmail.com>
2019-01-29 12:49:01 -0200

    Refatora crawler

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -7,23 +7,26 @@ import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-def get_raw_data():
-    url = "http://brumadinho.vale.com/listagem-pessoas-sem-contato.html"
-    r = requests.get(url)
-    r.encoding = 'utf-8'
-    return r.content
+class Crawler:
 
-def parse_html(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    text = soup.text
-    ultima_atualizacao = re.search('(?<=atualização em ).+(?=\.)', text).group()
-    ultima_atualizacao = ultima_atualizacao[:10]
-    nomes = [i.text.strip() for i in soup.find_all('li')]
-    return (ultima_atualizacao, nomes)
+    def get_raw_data(self):
+        url = "http://brumadinho.vale.com/listagem-pessoas-sem-contato.html"
+        r = requests.get(url)
+        r.encoding = 'utf-8'
+        return r.content
+
+    def parse_html(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        text = soup.text
+        ultima_atualizacao = re.search('(?<=atualização em ).+(?=\.)', text).group()
+        ultima_atualizacao = ultima_atualizacao[:10]
+        nomes = [i.text.strip() for i in soup.find_all('li')]
+        return (ultima_atualizacao, nomes)
 
 if __name__ == "__main__":
-    html = get_raw_data()
-    dt, pessoas = parse_html(html)
+    crawler = Crawler()
+    html = crawler.get_raw_data()
+    dt, pessoas = crawler.parse_html(html)
     
     with open('data.csv', encoding='utf-8') as csvfile:
         spamreader = csv.reader(csvfile)
