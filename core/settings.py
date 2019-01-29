6028bca05fa355d0ad89ad471310da80b3547b95
Rodrigo Gomes <rodgomesc@gmail.com>
2019-01-29 16:45:43 -0200

    the data csv moved to static for a while

--- core/settings.py
+++ core/settings.py
@@ -11,9 +11,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
