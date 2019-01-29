c13b52a0654a599b58bef0a3dc0ed2eb8fc4786f
Rodrigo Gomes <rodgomesc@gmail.com>
2019-01-29 18:26:10 -0200

    fixed suggestions

--- core/settings.py
+++ core/settings.py
@@ -11,8 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
