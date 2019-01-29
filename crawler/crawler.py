b664d76e5b773671ee4ea62f3890e979575f1500
BrunaDolavale <brunadolavalerj@gmail.com>
2019-01-29 12:35:25 -0200

    Corrige script csv das pessoas não encontradas

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -16,23 +16,22 @@ def get_raw_data():
 def parse_html(html):
     soup = BeautifulSoup(html, 'html.parser')
     text = soup.text
-    ultima_atualizacao = re.search('(?<=atualização em ).+(?=\.)'.decode('utf-8'), text).group()
-    ultima_atualizacao = datetime.strptime(ultima_atualizacao.encode('utf-8'), '%d/%m/%Y às %H:%M')
+    ultima_atualizacao = re.search('(?<=atualização em ).+(?=\.)', text).group()
+    ultima_atualizacao = ultima_atualizacao[:10]
     nomes = [i.text.strip() for i in soup.find_all('li')]
     return (ultima_atualizacao, nomes)
 
 if __name__ == "__main__":
     html = get_raw_data()
     dt, pessoas = parse_html(html)
-    dt = dt.strftime('%d/%m/%Y - %H:%M')
-	
-    with open('data.csv', 'rb') as csvfile:
+    
+    with open('data.csv', encoding='utf-8') as csvfile:
         spamreader = csv.reader(csvfile)
-        with open('data_compare.csv', 'w') as f:
+        with open('data_compare.csv', 'w', encoding='utf-8') as f:
             csv_writer = csv.writer(f)
             csv_writer.writerow(('Nome', 'Última Atualização', 'Status'))
             for row in spamreader:
-                t = 'Encontrado'
+                t = 'Não Encontrado'
                 for pessoa in pessoas:
                     if row[0] == pessoa.encode('utf-8'):
                         t = ' - '
