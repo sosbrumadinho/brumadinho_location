46d36bd93eface491c668583a4a12a23b373e0e1
raphael <raphael.costa@birmind.com.br>
2019-01-29 14:01:28 -0200

    tests for frontend

--- map/tests.py
+++ map/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestFrontEnd(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.response = self.c.get(reverse('map:map'))
+
+    def test_endpoint_result(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template_used(self):
+        self.assertTemplateUsed('index.html')
+
