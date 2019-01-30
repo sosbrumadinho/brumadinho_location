88fb1b436330465625c822486035a02a8ade58b6
Henrique Miranda <henrique@disroot.org>
2019-01-30 16:26:26 -0200

    Add function to return elevation of a point

--- api/views.py
+++ api/views.py
@@ -5,6 +5,8 @@ from rest_framework.views import APIView
 from api.serializers import CoordinateSerializer
 from api.utils import Position
 
+from django.conf import settings
+
 
 class CalculateCoordinate(APIView):
     """
@@ -20,5 +22,10 @@ class CalculateCoordinate(APIView):
         vector_position = Position(lat, lng).calc_vector()
         return Response(vector_position, status=status.HTTP_200_OK)
 
+
+def get_elevation(lat, lng):
+    gmaps = googlemaps.Client(key=settings.GMAPS_API_KEY)
+    geocode_result = gmaps.elevation((lat, lng))
+    return geocode_result[0]['elevation']
 
 calculatecoordinate = CalculateCoordinate.as_view()
