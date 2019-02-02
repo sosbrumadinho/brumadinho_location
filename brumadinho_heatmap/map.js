a23dd5e483efbb577c4287af31b240df25933772
José Bezerra de Melo Neto <josebezerra@Joses-MacBook-Air-2.local>
2019-02-02 13:57:43 -0300

    beta 1

--- brumadinho_heatmap/map.js
+++ brumadinho_heatmap/map.js
@@ -1,5 +1,21 @@
+var mapData;
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9zZWJlemVycmEiLCJhIjoiY2pyazVtdDA5MDE5czQ0cmdsNnFjZjVsdiJ9.g3x0Z17jXKFjfYEc2ivxsg';
+
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://brumadinho-api.herokuapp.com/api/all_places', true);
+
+request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        mapData = JSON.parse(this.response);
+
+    } else {
+        console.log('error');
+    }
+}
+
+request.send();
 
 var map = new mapboxgl.Map({
     container: 'map',
@@ -12,7 +28,8 @@ var map = new mapboxgl.Map({
 map.on('load', function () {
     map.addSource('earthquakes', {
         "type": "geojson",
-        "data": "./misc/coords.geojson"
+        // "data": "./misc/coords.geojson"
+        "data": mapData
     });
 
     map.addLayer({
@@ -30,16 +47,8 @@ map.on('load', function () {
 
 });
 
-// var marker = new mapboxgl.Marker({
-//     draggable: true
-// })
-//     .setLngLat([0, 0])
-//     .addTo(map);
-
-// function onDragEnd() {
-//     var lngLat = marker.getLngLat();
-//     coordinates.style.display = 'block';
-//     // coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
-// }
-
-// marker.on('dragend', onDragEnd);
+map.on('mousemove', function (e) {
+    let long = e.lngLat.lng
+    let lat = e.lngLat.lat
+    document.getElementById('info').innerHTML = "Latitude: "+lat+" Longitude: "+long
+});
